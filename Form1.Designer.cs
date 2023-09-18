7016bb11b35c7ccdfd961d0758a1414e8002bd80
dfergisz <daniel.fergisz@opi.org.pl>
2023-09-18 16:29:55 +0200

    v1.08 - update banner

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -110,7 +110,7 @@ namespace PS_Updater_X
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "PsFirm Updater v1.07";
+            this.Text = "PsFirm Updater v1.08";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
